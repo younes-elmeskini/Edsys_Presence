a7c4b8db0c505fcc9dbd8ef7016b8c373b10b262
Elmeskini Youness <yns.elmeskini@gmail.com>
2025-05-19 17:04:16 +0100

    feat: Initialize project with Express, Prisma, and TypeScript setup
    - Added package.json with dependencies and scripts for development and production.
    - Created Prisma schema for Teacher, Student, Session, StudentAbsence, and QRcode models.
    - Implemented basic structure for Student and Teacher routes and controllers.
    - Set up Express application with middleware for security, rate limiting, and CORS.
    - Added JWT authentication middleware for user verification.
    - Created utility functions for Prisma client and email sending with Nodemailer.
    - Defined Zod validation schemas for user input validation.

---
+++ src/app.ts
@@ -0,0 +1,43 @@
+import express from "express";
+import TeacherRoute from "./Teacher/TeacherRoute";
+import StudentRoute from "./Student/StudentRoute";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization", "Set-Cookie"],
+  })
+);
+
+app.disable("x-powered-by");
+
+app.use(helmet());
+app.set("trust proxy", 1);
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 1000,
+});
+
+app.use(limiter);
+// Routes
+app.use("/teacher", TeacherRoute);
+
+app.use("/student", StudentRoute);
+
+export default app;
